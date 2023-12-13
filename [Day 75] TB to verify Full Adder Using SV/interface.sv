17da408d8b36c6abcf6751ffa46eaa315b227858
karribharathi <93812331+karribharathi@users.noreply.github.com>
2023-12-13 14:15:31 +0530

    Delete [Day 75] TB to verify Full Adder Using SV directory

--- [Day 75] TB to verify Full Adder Using SV/interface.sv
+++
@@ -1,8 +0,0 @@
-//interface
-interface fa_intf;
-logic in1;
-logic in2;
-logic cin;
-logic sum;
-logic cout;
-endinterface
--- [Day 75] TB to verify Full Adder Using SV/transaction.sv
+++
@@ -1,7 +0,0 @@
-class transaction;
-randc bit in1;
-randc bit in2;
-randc bit cin; 
-bit sum;
-bit cout;
-endclass
