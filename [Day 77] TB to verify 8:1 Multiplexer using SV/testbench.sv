ba2cc37a6df68aff2ddff961016035b9940974d6
karribharathi <93812331+karribharathi@users.noreply.github.com>
2023-12-14 18:53:04 +0530

    Create testbench.sv

---
+++ [Day 77] TB to verify 8:1 Multiplexer using SV/testbench.sv
@@ -0,0 +1,17 @@
+//top.sv
+`include "interface.sv"
+`include "test.sv"
+
+ module testbench;
+ 
+ mux_if pif();
+ test tb(pif);
+ 
+   mux m1(.d(pif.d),.sel(pif.sel),.y(pif.y));
+ initial
+ begin
+ $dumpfile("dump.vcd");
+ $dumpvars(1);
+ 
+ end
+ endmodule
