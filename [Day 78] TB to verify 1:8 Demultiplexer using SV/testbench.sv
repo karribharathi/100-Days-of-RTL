b454a53e5bf3ed07381b1a6c4f1a45ee2a5d51c5
karribharathi <93812331+karribharathi@users.noreply.github.com>
2023-12-15 21:49:00 +0530

    Create testbench.sv

---
+++ [Day 78] TB to verify 1:8 Demultiplexer using SV/testbench.sv
@@ -0,0 +1,17 @@
+
+`include "interface.sv"
+`include "test.sv"
+
+ module testbench;
+ 
+ demux_if pif();
+ test tb(pif);
+ 
+   demux m1(.d(pif.d),.sel(pif.sel),.y(pif.y));
+ initial
+ begin
+ $dumpfile("dump.vcd");
+ $dumpvars(1);
+ 
+ end
+ endmodule
